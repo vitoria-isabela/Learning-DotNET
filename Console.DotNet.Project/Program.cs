f7682c772a7d19b9e9f89e58bb75820d0f2b8093
vitoria-isabela <vitoriaisabela368@gmail.com>
2022-04-14 09:30:04 -0300

    Sending kickoff projects.

---
+++ Console.DotNet.Project/Program.cs
@@ -0,0 +1,41 @@
+﻿using Humanizer;
+using System.Diagnostics;
+using System.IO;
+
+//Console.WriteLine("Quantities:");
+//HumanizeQuantities();
+
+//Console.WriteLine("\nDate/Time Manipulation:");
+//HumanizeDates();
+
+//static void HumanizeQuantities()
+//{
+//    Console.WriteLine("case".ToQuantity(0));
+//    Console.WriteLine("case".ToQuantity(1));
+//    Console.WriteLine("case".ToQuantity(5));
+//}
+
+//static void HumanizeDates()
+//{
+//    Console.WriteLine(DateTime.UtcNow.AddHours(-24).Humanize());
+//    Console.WriteLine(DateTime.UtcNow.AddHours(-2).Humanize());
+//    Console.WriteLine(TimeSpan.FromDays(1).Humanize());
+//    Console.WriteLine(TimeSpan.FromDays(16).Humanize());
+//}
+
+IEnumerable<string> listOfDirectories = Directory.EnumerateDirectories("C:\\Users\\6113826\\source\\repos\\Learning-DotNET\\Console.DotNet.Project");
+IEnumerable<string> listOfFiles = Directory.EnumerateFiles("C:\\Users\\6113826\\source\\repos\\Learning-DotNET\\Console.DotNet.Project");
+
+Console.WriteLine("Directories: ");
+foreach (var dir in listOfDirectories)
+{
+    Console.WriteLine(dir);
+}
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Files: ");
+foreach (var file in listOfFiles)
+{
+    Console.WriteLine(file);
+}
---
+++ mslearn-dotnet-files/Program.cs
@@ -0,0 +1,26 @@
+﻿var currentDirectory = Directory.GetCurrentDirectory();
+var storesDirectory = Path.Combine(currentDirectory, "stores");
+
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+Directory.CreateDirectory(salesTotalDir);     
+var salesFiles = FindFiles(storesDirectory);
+
+File.WriteAllText(Path.Combine(salesTotalDir, "totals.txt"), String.Empty);
+
+IEnumerable<string> FindFiles(string folderName)
+{
+    List<string> salesFiles = new List<string>();
+
+    var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+
+    foreach (var file in foundFiles)
+    {
+        var extension = Path.GetExtension(file);
+        if (extension == ".json")
+        {
+            salesFiles.Add(file);
+        }
+    }
+
+    return salesFiles;
+}
